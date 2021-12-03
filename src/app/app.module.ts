87e233131cbac07781005d0578a350afcb3dcdf9
Pinheiro <taysa.pinheiro@capgemini.com>
2021-12-02 22:06:23 -0300

    correção footer e icone de pesquisa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
